f93ae75d16dd47f138aa9408ddd388a5daf49c6c
blucas6 <66273291+blucas6@users.noreply.github.com>
2022-11-04 15:51:05 -0400

    feature: local copy of statements created

---
+++ src/application.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+import shutil
+import os
+
+from config import *
+from homepage import Homepage
+from sessionpage import Sessionpage
+
+class Application(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.app_container = tk.Frame(self)
+        self.app_container.pack(side="top", fill="both", expand=True)
+        self.app_container.grid_rowconfigure(0, weight=1)
+        self.app_container.grid_columnconfigure(0, weight=1)
+
+        self.menubar = tk.Menu(self.app_container)
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Import Data", command=lambda:self.ImportData())
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="Exit", command=quit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        tk.Tk.config(self, menu=self.menubar)
+
+        self.app_frames = {}
+        for F in (Sessionpage, Homepage):
+            frame = F(self.app_container, self)
+            self.app_frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.ShowFrame(Homepage)
+
+    def ShowFrame(self, cont):
+        f = self.app_frames[cont]
+        f.tkraise()
+
+    def ImportData(self):
+        filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files","*.txt*"),("all files","*.*")))
+        shutil.copy(filename, STATEMENT_DIR)
--- src/main.py
+++ src/main.py
@@ -1,31 +1,20 @@
-import tkinter as tk
+import os
 
+from application import Application
 from config import *
-from homepage import Homepage
-from sessionpage import Sessionpage
 
 
-class Application(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-        self.app_container = tk.Frame(self)
-        self.app_container.pack(side="top", fill="both", expand=True)
-        self.app_container.grid_rowconfigure(0, weight=1)
-        self.app_container.grid_columnconfigure(0, weight=1)
 
-        self.app_frames = {}
-        for F in (Sessionpage, Homepage):
-            frame = F(self.app_container, self)
-            self.app_frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.showFrame(Homepage)
-
-    def showFrame(self, cont):
-        f = self.app_frames[cont]
-        f.tkraise()
+def Clean():
+    if not os.path.exists(STATEMENT_DIR):
+        os.makedirs(STATEMENT_DIR)
 
 
+##############################################################
+############## RUN SEQUENCE ##################################
+Clean()
 app = Application()
 app.geometry("%sx%s"%(WIN_WIDTH, WIN_HEIGHT))
 app.mainloop()
+
+##############################################################
